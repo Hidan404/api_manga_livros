5305145f0b78cb8c65608978a646967cb98f0e1e
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2025-11-24 19:29:27 -0300

    feat(SenhaHash): implementar classe com metodos que geram o hash e verifiquem a senha junto ao hash
    BREAKING CHANGE:

---
+++ app/utils/senha-hasher.py
@@ -0,0 +1,19 @@
+from passlib.context import CryptContext
+
+pwd_contexto = CryptContext(schemes=["bcrypt"])
+
+class SenhaHash():
+    
+    @staticmethod
+    def hash_criar(senha: str) -> str:
+        senha2 = senha[:72]
+        print(type(pwd_contexto.hash(senha2)))
+        return pwd_contexto.hash(senha2)
+    
+    @staticmethod
+    def verificar_senha(senha: str, senha_hash: str) -> bool:
+        return pwd_contexto.verify(senha, senha_hash)
+    
+
+s = SenhaHash()
+s.hash_criar("hidanexagonal")    
