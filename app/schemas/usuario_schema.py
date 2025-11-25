28992d2c9ed1072cae2a12eee8b774f8f93344bf
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2025-11-24 21:06:59 -0300

    feat(autentica_schema): Criar schema para login e liberação para o teken acesso
    BREAKING CHANGE:

--- app/schemas/usuario_schema.py
+++ app/schemas/usuario_schema.py
@@ -4,3 +4,11 @@ class UsuarioCriar(BaseModel):
     nome: str
     email: EmailStr
     senha: str
+
+class UsuarioResposta(BaseModel):
+    id: int
+    nome: str
+    email: EmailStr
+
+    class Config:
+        para_atributos = True
