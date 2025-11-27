e3751f15e79664d2b1ae89714f2c1caac7ab6dfe
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2025-11-27 20:50:30 -0300

    fix(main,manga_rotas): manga_rotas ao reusar codigo esqueci de mudar as variaveis ,adicionei imports no main
    BREAKING CHANGE:

--- app/routers/rotas_mangas.py
+++ app/routers/rotas_mangas.py
@@ -2,31 +2,32 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from app.database.conexao import get_db
-from app.schemas.livro_schemas import LivroCreate, LivroUpdate
-from app.controllers.livro_controller import LivroController
+from app.schemas.manga_schemas import MangaCreate, MangaUpdate
+from app.controllers.manga_controller import MangaController
 
-rota_livros = APIRouter(prefix="/livros", tags=["Livros"])
-
-
-@rota_livros.get("/")
-def listar_livros(db: Session = Depends(get_db)):
-    return LivroController.listar(db)
+rota_mangas = APIRouter(prefix="/mangas", tags=["Mangas"])
 
 
-@rota_livros.get("/{livro_id}")
-def obter_livro(livro_id: int, db: Session = Depends(get_db)):
-    return LivroController.obter_por_id(db, livro_id)
+@rota_mangas.get("/")
+def listar_mangas(db: Session = Depends(get_db)):
+    return MangaController.listar(db)
 
 
-@rota_livros.post("/")
-def criar_livro(dados: LivroCreate, db: Session = Depends(get_db)):
-    return LivroController.criar(db, dados)
+@rota_mangas.get("/{manga_id}")
+def obter_manga(manga_id: int, db: Session = Depends(get_db)):
+    return MangaController.obter_por_id(db, manga_id)
 
 
-@rota_livros.put("/{livro_id}")
-def atualizar_livro(livro_id: int, dados: LivroUpdate, db: Session = Depends(get_db)):
-    return LivroController.atualizar(db, livro_id, dados)
+@rota_mangas.post("/")
+def criar_manga(dados: MangaCreate, db: Session = Depends(get_db)):
+    return MangaController.criar(db, dados)
 
-@rota_livros.delete("/{livro_id}")
-def deletar_livro(livro_id: int, db: Session = Depends(get_db)):
-    return LivroController.deletar(db, livro_id)
+
+@rota_mangas.put("/{manga_id}")
+def atualizar_manga(manga_id: int, dados: MangaUpdate, db: Session = Depends(get_db)):
+    return MangaController.atualizar(db, manga_id, dados)
+
+
+@rota_mangas.delete("/{manga_id}")
+def deletar_manga(manga_id: int, db: Session = Depends(get_db)):
+    return MangaController.deletar(db, manga_id)
