f834a456a7e85b22cfa10156edef800146e7bd67
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2025-11-27 21:16:16 -0300

    fix(rotas_livros): Corrigir rotas adicionando autenticação
    BREAKING CHANGE:

--- app/main.py
+++ app/main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
 from app.database.conexao import Base, criacao as engine
 from app.routers.rotas_autentica import rota as auth_router
+from app.routers.rotas_livros import rota_livros
+from app.routers.rotas_mangas import rota_mangas
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 app.include_router(auth_router)
+app.include_router(rota_livros)
+app.include_router(rota_mangas)
 
 if __name__ == "__main__":
     import uvicorn
--- app/routers/rotas_livros.py
+++ app/routers/rotas_livros.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from app.database.conexao import get_db
 from app.schemas.livro_schemas import LivroCreate, LivroUpdate
 from app.controllers.livro_controller import LivroController
+from app.core.dependecia_auth import obter_usuario_logado, require_role
 
 rota_livros = APIRouter(prefix="/livros", tags=["Livros"])
 
@@ -18,16 +19,16 @@ def obter_livro(livro_id: int, db: Session = Depends(get_db)):
     return LivroController.obter_por_id(db, livro_id)
 
 
-@rota_livros.post("/")
-def criar_livro(dados: LivroCreate, db: Session = Depends(get_db)):
+@rota_livros.post("/", dependencies=[Depends(require_role("admin"))])
+def criar_livro(dados: LivroCreate, db: Session = Depends(get_db), usuario_logado=Depends(obter_usuario_logado)):
     return LivroController.criar(db, dados)
 
 
-@rota_livros.put("/{livro_id}")
-def atualizar_livro(livro_id: int, dados: LivroUpdate, db: Session = Depends(get_db)):
+@rota_livros.put("/{livro_id}", dependencies=)
+def atualizar_livro(livro_id: int, dados: LivroUpdate, db: Session = Depends(get_db), usuario_logado=Depends(obter_usuario_logado)):
     return LivroController.atualizar(db, livro_id, dados)
 
 
-@rota_livros.delete("/{livro_id}")
-def deletar_livro(livro_id: int, db: Session = Depends(get_db)):
+@rota_livros.delete("/{livro_id}", dependencies=[Depends(require_role("admin"))])
+def deletar_livro(livro_id: int, db: Session = Depends(get_db), usuario_logado=Depends(obter_usuario_logado)):
     return LivroController.deletar(db, livro_id)
