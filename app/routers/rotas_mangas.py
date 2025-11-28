7fcf1ccbc2adb0652d9edff8ce81cbe7c18a58b5
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2025-11-27 21:21:02 -0300

    refactor(rotas_mangas): Adicionar dependencias para autenticação
    BREAKING CHANGE:

--- app/routers/rotas_mangas.py
+++ app/routers/rotas_mangas.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from app.database.conexao import get_db
 from app.schemas.manga_schemas import MangaCreate, MangaUpdate
 from app.controllers.manga_controller import MangaController
+from app.core.dependecia_auth import obter_usuario_logado, require_role
 
 rota_mangas = APIRouter(prefix="/mangas", tags=["Mangas"])
 
@@ -18,16 +19,16 @@ def obter_manga(manga_id: int, db: Session = Depends(get_db)):
     return MangaController.obter_por_id(db, manga_id)
 
 
-@rota_mangas.post("/")
-def criar_manga(dados: MangaCreate, db: Session = Depends(get_db)):
+@rota_mangas.post("/", dependencies=[Depends(require_role("admin"))])
+def criar_manga(dados: MangaCreate, db: Session = Depends(get_db), usuario_logado=Depends(obter_usuario_logado)):
     return MangaController.criar(db, dados)
 
 
-@rota_mangas.put("/{manga_id}")
-def atualizar_manga(manga_id: int, dados: MangaUpdate, db: Session = Depends(get_db)):
+@rota_mangas.put("/{manga_id}", dependencies=[Depends(require_role("admin"))])
+def atualizar_manga(manga_id: int, dados: MangaUpdate, db: Session = Depends(get_db), usuario_logado=Depends(obter_usuario_logado)):
     return MangaController.atualizar(db, manga_id, dados)
 
 
-@rota_mangas.delete("/{manga_id}")
-def deletar_manga(manga_id: int, db: Session = Depends(get_db)):
+@rota_mangas.delete("/{manga_id}", dependencies=[Depends(require_role("admin"))])
+def deletar_manga(manga_id: int, db: Session = Depends(get_db), usuario_logado=Depends(obter_usuario_logado)):
     return MangaController.deletar(db, manga_id)
