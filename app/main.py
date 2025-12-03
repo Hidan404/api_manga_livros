c5980d67eadda647cb78d13efeb4cf899dbcb952
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2025-12-02 23:05:38 -0300

    feat(registro): implementar rota de registro de usuário e atualizar utilitário de hash de senha
    BREAKING CHANGE:

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from app.routers.rotas_livros import rota_livros
 from app.routers.rotas_mangas import rota_mangas
 from app.routers.rotas_favoritos_mangas import rota_favoritos_manga
 from app.routers.rotas_favoritos_livros import routa_favoritos_livros
+from app.routers.rota_registro import rota as rota_registro
 
 Base.metadata.create_all(bind=engine)
 
@@ -15,6 +16,7 @@ app.include_router(rota_livros)
 app.include_router(rota_mangas)
 app.include_router(rota_favoritos_manga)
 app.include_router(routa_favoritos_livros)
+app.include_router(rota_registro)
 
 if __name__ == "__main__":
     import uvicorn
