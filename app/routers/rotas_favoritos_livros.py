2b6b58e0760a6c07611f44890a3b539dcb623d4f
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2025-12-09 21:22:45 -0300

    fix(rotas_favoritos_livros): Corrigindo rotas dos favoritos dos livros e adicionando parametro nas funções de usuario
    BREAKING CHANGE:

--- app/routers/rotas_favoritos_livros.py
+++ app/routers/rotas_favoritos_livros.py
@@ -1,20 +1,33 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from app.database.conexao import get_db
 from app.controllers.favoritos_controller import FavoritoLivroController
 from app.utils.dependecias_utils import get_current_user
-#Todo: criar controller para favoritos de livros
+
 routa_favoritos_livros = APIRouter(prefix="/favoritos/livros", tags=["Favoritos - Livros"])
 
 
-@routa_favoritos_livros.post("/", dependencies=[Depends(get_current_user)])
-def adicionar_favorito_livro(livro_id: int, usuario=Depends(get_current_user)):
-    return FavoritoLivroController.adicionar_favorito(usuario["id"], livro_id)
+@routa_favoritos_livros.post("/{livro_id}")
+def adicionar_favorito_livro(
+    livro_id: int,
+    db: Session = Depends(get_db),
+    usuario=Depends(get_current_user)
+):
+    return FavoritoLivroController.adicionar_favorito(db, usuario["id"], livro_id)
 
 
-@routa_favoritos_livros.get("/", dependencies=[Depends(get_current_user)])
-def listar_favoritos_livros(usuario=Depends(get_current_user)):
-    return FavoritoLivroController.listar_favoritos(usuario["id"])
+@routa_favoritos_livros.get("/")
+def listar_favoritos_livros(
+    db: Session = Depends(get_db),
+    usuario=Depends(get_current_user)
+):
+    return FavoritoLivroController.listar_favoritos(usuario["id"], db)
 
 
-@routa_favoritos_livros.delete("/{favorito_id}", dependencies=[Depends(get_current_user)])
-def remover_favorito_livro(favorito_id: int, usuario=Depends(get_current_user)):
-    return FavoritoLivroController.remover_favorito(usuario["id"], favorito_id)
+@routa_favoritos_livros.delete("/{favorito_id}")
+def remover_favorito_livro(
+    favorito_id: int,
+    db: Session = Depends(get_db),
+    usuario=Depends(get_current_user)
+):
+    return FavoritoLivroController.remover_favorito(usuario["id"], favorito_id, db)
