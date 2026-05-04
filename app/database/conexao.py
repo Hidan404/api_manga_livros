2bd639c6e938c23a68e7a913d0d8213d936dfbb1
ronald <ronaldkurouzo@gmail.com>
2026-05-04 09:40:41 -0300

    fix: corrigir Dockerfile e env

--- app/database/conexao.py
+++ app/database/conexao.py
@@ -2,15 +2,35 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 import os
+import time
 
 load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-criacao = create_engine(SQLALCHEMY_DATABASE_URL)
+# 🔥 Função que espera o banco subir
+def criar_engine_com_retry():
+    for i in range(10):
+        try:
+            engine = create_engine(SQLALCHEMY_DATABASE_URL)
+            connection = engine.connect()
+            connection.close()
+            print("✅ Banco conectado!")
+            return engine
+        except Exception as e:
+            print(f"⏳ Tentando conectar no banco... ({i+1}/10)")
+            time.sleep(3)
+    
+    raise Exception("❌ Não conseguiu conectar no banco")
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=criacao)
+# usa o retry aqui
+criacao = criar_engine_com_retry()
 
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=criacao
+)
 
 Base = declarative_base()
 
